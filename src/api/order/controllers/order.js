d37ec9bdf7ffc1090538e4a3d0c8f92ed5283b6c
dnieto1997 <121445107+dnieto1997@users.noreply.github.com>
2024-08-21 14:40:41 -0500

    act

---
+++ src/api/order/controllers/order.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * order controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async paymentOrder(ctx) {
+    ctx.body = "pago y pedido generado correctamente";
+  },
+}));
